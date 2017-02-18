04dad4ee2b25e3cf029062f12f3f00eb42cf44a0
Zac Lang <zac.wr.lang@gmail.com>
2017-02-18 14:55:24 +1100

    allow users to set a city/location for their search

--- test/zomato.spec.js
+++ test/zomato.spec.js
@@ -6,11 +6,11 @@ describe('ZomatoService', function() {
   describe('Search Zomato for "hoffa"', function () {
     it('Search Zomato for "hoffa"', function (done) {
       this.timeout(5000);
-      zomatoService.search('hoffa praha').then((res) => {
-        expect(res).to.be.an('object')
-        expect(res.results_found).to.be.at.least(1)
-        expect(res.restaurants).to.be.an('array')
-        expect(res.restaurants).to.have.length.above(0)
+      zomatoService.searchRestaurants('hoffa praha').then((res) => {
+        expect(res).to.be.an('object');
+        expect(res.results_found).to.be.at.least(1);
+        expect(res.restaurants).to.be.an('array');
+        expect(res.restaurants).to.have.length.above(0);
 
 
         done();
