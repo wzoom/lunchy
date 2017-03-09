df8926494e6db309526375321ed9cedb4216ca5a
Pavol Otcenas <pavol@otcenas.eu>
2017-03-09 08:35:21 +0100

    Fix docs

--- scripts/lunchy.js
+++ scripts/lunchy.js
@@ -7,7 +7,7 @@
 // Commands:
 //   hubot location <city> - Search and set your location.
 //   hubot search <restaurant> - Add Restaurant(s) by Searching and choosing from list.
-//   hubot list - List Daily Menus of added restaurants
+//   hubot menus - List Daily Menus of added restaurants
 //   hubot remove <restaurant> - Search for restaurant to remove and asks for removal.
 //
 
